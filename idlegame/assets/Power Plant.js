7c36e2db80eeebf9b18711599aaf6ea6f92768cf
Hisham Alsamarrai <hishamalsamarrai@Hishams-MacBook-Pro.local>
2019-10-09 21:30:04 -0500

    I made a power plant component

---
+++ idlegame/assets/Power Plant.js
@@ -0,0 +1,35 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // The power plants carbon footprint
+        carbonFootprint: 0,
+
+        // The power plants rate of electricity
+        rateOfElectricity: 0,
+
+        // The power plants cost to build
+        costToBuild: 0,
+
+        // The power plants time to build
+        timeToBuild: 0,
+    },
+
+
+
+    start () 
+    {
+        
+    },
+
+});
